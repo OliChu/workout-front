2516262366db2ee6410926373b71d33c7ef7e564
Olivier Chuiton <olivier.chuiton@gmail.com>
2020-10-25 20:42:59 +0100

    rename folder and working compilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,16 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// const isProduction = process.env.NODE_ENV === 'production'
-
 module.exports = {
-  entry: path.resolve(__dirname, '../src/index.js'),
+  entry: path.resolve(__dirname, './app/index.js'),
   output: {
-    path: path.resolve(__dirname, '../dist'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'app_bundle.js',
   },
-  // watch: !isProduction,
-  mode: "development",
   module: {
     rules: [
       { 
-        test: /\.(js)$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
@@ -38,11 +34,11 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: './app/index.html'
     })
   ],
   devServer: {
-    contentBase: path.join(__dirname, '../dist'),
+    contentBase: path.join(__dirname, './dist'),
     port: 8080,
   }
 }
